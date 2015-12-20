2b0caf406b512f62aabdd6d720aba389eedc5cc2
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-12-20 15:28:40 -0800

    First commit

---
+++ catastrophe_clock/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import TemplateView
+
+
+class ClockView(TemplateView):
+    """
+    The view for the main page -- a clock that counts down until some catastrophe has been calculated to occur.
+    """
+    pass
