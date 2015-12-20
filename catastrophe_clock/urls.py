0901b29c70f396866390a4e7e1fa319e85441b67
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-12-20 15:33:24 -0800

    real fixup

--- catastrophe_clock/urls.py
+++ catastrophe_clock/urls.py
@@ -20,5 +20,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', TemplateView.as_view(template_name="clock.html")),
+    url(r'', TemplateView.as_view(template_name="catastrophe_clock/clock.html")),
 ]
