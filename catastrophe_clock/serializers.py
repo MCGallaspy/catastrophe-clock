2f1eb283672eb860b26feb55b08bdfcbc72c343d
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-12-20 19:20:27 -0800

    Fixup

--- catastrophe_clock/serializers.py
+++ catastrophe_clock/serializers.py
@@ -7,6 +7,7 @@ from .models import Catastrophe
 class CatastropheSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Catastrophe
+        fields = ('name', 'description', 'arrival_date', 'url', )
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
