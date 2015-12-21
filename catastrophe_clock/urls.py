0c86914190e7d4ae064adbb811ec8a21727d02aa
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-12-20 18:39:47 -0800

    Add django rest framework skeleton

--- catastrophe_clock/urls.py
+++ catastrophe_clock/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', TemplateView.as_view(template_name="catastrophe_clock/clock.html")),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
